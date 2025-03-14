7595d0ba96f828349b1e476c746976e469edd2f4
Kulsgam <kuls.gam@gmail.com>
2025-03-15 00:03:24 +1100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh dark:bg-yellow-400 bg-red-500">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
