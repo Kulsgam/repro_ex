e3840226adab404865b35e57113399af577d1198
Kulsgam <90457704+Kulsgam@users.noreply.github.com>
2025-03-14 13:10:31 +0000

    Set primary port

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { Button } from "@/components/ui/button";
-
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh dark:bg-yellow-400 bg-red-500">
-      <Button>Click me</Button>
-    </div>
+    <div className="flex flex-col items-center justify-center min-h-svh dark:bg-yellow-400 bg-red-500"></div>
   );
 }
 
